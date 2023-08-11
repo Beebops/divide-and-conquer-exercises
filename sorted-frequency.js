ca1003f5d985acc11c90d2496b3e0f224748fd5d
Beth Carretta <bethcarretta@gmail.com>
2023-08-11 09:05:07 -0400

    sorted-frequency passing tests

--- sorted-frequency.js
+++ sorted-frequency.js
@@ -5,6 +5,64 @@ sortedFrequency([1,1,2,2,2,2,3],3) // 1
 sortedFrequency([1,1,2,2,2,2,3],1) // 2
 sortedFrequency([1,1,2,2,2,2,3],4) // -1
  */
-function sortedFrequency() {}
+
+function findFirstOccurrence(arr, target) {
+  let left = 0
+  let right = arr.length - 1
+  let result = -1
+
+  while (left <= right) {
+    let middle = Math.floor((left + right) / 2)
+    let value = arr[middle]
+
+    if (value === target) {
+      // found the target, but need to still find first occurence
+      result = middle // update the result
+      right = middle - 1 // search left half of array
+    } else if (value > target) {
+      right = middle - 1 // target is in left half of array
+    } else {
+      left = middle + 1 // target is in right half of array
+    }
+  }
+  return result
+}
+
+function findLastOccurrence(arr, target) {
+  let left = 0
+  let right = arr.length - 1
+  let result = -1
+
+  while (left <= right) {
+    let middle = Math.floor((left + right) / 2)
+    let value = arr[middle]
+
+    if (value === target) {
+      result = middle
+      left = middle + 1
+    } else if (value > target) {
+      right = middle - 1
+    } else {
+      left = middle + 1
+    }
+  }
+  return result
+}
+
+console.log(sortedFrequency([1, 1, 2, 2, 2, 2, 3], 2)) // 4
+console.log(sortedFrequency([1, 1, 2, 2, 2, 2, 3], 3)) // 1
+console.log(sortedFrequency([1, 1, 2, 2, 2, 2, 3], 1)) // 2
+console.log(sortedFrequency([1, 1, 2, 2, 2, 2, 3], 4)) // -1
+
+function sortedFrequency(arr, target) {
+  let firstOccurrence = findFirstOccurrence(arr, target)
+  let lastOccurrence = findLastOccurrence(arr, target)
+
+  if (firstOccurrence === -1 || lastOccurrence === -1) {
+    return -1
+  } else {
+    return lastOccurrence - firstOccurrence + 1
+  }
+}
 
 module.exports = sortedFrequency
