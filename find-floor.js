943d879bd500f480435427d1e127e0db1404f267
Beth Carretta <bethcarretta@gmail.com>
2023-08-11 09:57:22 -0400

    find-floor passing

--- find-floor.js
+++ find-floor.js
@@ -3,10 +3,40 @@ Write a function called findFloor which accepts a sorted array and a value x, an
 
 Examples:
 
-findFloor([1,2,8,10,10,12,19], 9) // 8
+findFloor([1,2,8,10,10,12,19], 9) // 8 
 findFloor([1,2,8,10,10,12,19], 20) // 19
-findFloor([1,2,8,10,10,12,19], 0) // -1 */
+findFloor([1,2,8,10,10,12,19], 0) // -1
 
-function findFloor() {}
+*/
+
+function findFloor(arr, num) {
+  let left = 0
+  let right = arr.length - 1
+  let result = -1
+
+  if (num < arr[0]) {
+    return -1
+  }
+
+  while (left <= right) {
+    let middle = Math.floor((left + right) / 2)
+    let value = arr[middle]
+
+    if (num === value) {
+      // we found the floor!
+      return value
+    } else if (num > value) {
+      // update result and search right half of array
+      result = value
+      left = middle + 1
+    } else {
+      // num is smaller, so search left half of array
+      right = middle - 1
+    }
+  }
+  return result
+}
+
+//console.log(findFloor([1, 2, 8, 10, 10, 12, 19], 5))
 
 module.exports = findFloor
